c9a7598645aea784af4fee8d02828a706fee8016
monisharamaiah <monirramaiah@gmail.com>
2024-12-15 21:54:37 +0530

    Create isupper,islower.py

---
+++ isupper,islower.py
@@ -0,0 +1,16 @@
+sentence = input("Enter a sentence: ")
+words = digits = upper = lower = 0
+split_sentence = sentence.split()
+print("The result of split() on input sentence is : \n"+str(split_sentence)+"\n")
+words = len(split_sentence )
+for c in sentence:
+  if c.isdigit():
+   digits = digits + 1
+  elif c.isupper():
+   upper = upper + 1
+  elif c.islower():
+   lower = lower + 1
+print ("No of Words: ", words)
+print ("No of Digits: ", digits)
+print ("No of Uppercase letters: ", upper)
+print ("No of Lowercase letters: ", lower)
